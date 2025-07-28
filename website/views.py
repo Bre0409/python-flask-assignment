7ec993bbb3db0f1d751b03311b4aa54318340fac
Bre0409 <brendanboland@hotmail.co.uk>
2025-07-28 13:58:27 +0100

    mq

--- website/views.py
+++ website/views.py
@@ -1,18 +1,15 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from .events import events  # Make sure events is defined/exported from events.py
+from .events import events 
 
 views = Blueprint('views', __name__)
 
-# In-memory storage for tasks (temporary)
+
 tasks = []
 task_id_counter = 1
 
-# Supported day labels
 DAY_NAMES = ["Today", "Tomorrow", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-# ----------------------
-# Task Management Routes
-# ----------------------
+
 
 @views.route('/tasks', methods=['GET', 'POST'])
 def task_list():
@@ -66,17 +63,14 @@ def edit_task(task_id):
             break
     return redirect(request.referrer or url_for('views.task_list'))
 
-# ----------------------
-# Navigation & Calendar
-# ----------------------
-
 @views.route('/')
 def index():
-    # Redirect root to the event-adding route
-    return redirect(url_for('events.add_event'))
+    # Redirect root to the home page 
+    return redirect(url_for('auth.home'))
 
 @views.route('/calendar')
 def calendar():
     return render_template('calendar.html', events=events)
+
 
 
